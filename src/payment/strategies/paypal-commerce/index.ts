6ea172d0bf7e22d8638f3cdb8302c9a051d8bda4
Olga Lashkul <olga.lashkul@bigcommerce.com>
2020-01-09 11:11:46 +0200

    feat(payment): PAYPAL_COMMERCE

---
+++ src/payment/strategies/paypal-commerce/index.ts
@@ -0,0 +1,5 @@
+export * from './paypal-commerce-sdk';
+
+export { default as PaypalCommerceScriptLoader } from './paypal-commerce-script-loader';
+export { default as PaypalCommercePaymentStrategy } from './paypal-commerce-payment-strategy';
+export { default as PaypalCommerceRequestSender } from './paypal-commerce-request-sender';
